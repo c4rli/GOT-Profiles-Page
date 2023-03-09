ee21c7d9a9d9f9b1d19f66aff25f819a75b373bc
xarli <117117311+c4rli@users.noreply.github.com>
2023-03-09 20:51:18 +0000

    Add alt tag to image

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,11 +1,10 @@
 import React from "react";
-import characterData from "../data/characterData.json"
 
 const Character = ({name, imgUrl, birth, death, race, realm , spouse}) => {
     return (
         <div data-test="component-character">
             <h1 data-test="char-name">{name}</h1>
-            <img data-test="char-img" alt="" src={imgUrl}/>
+            <img data-test="char-img" alt={name} src={imgUrl}/>
             <ul data-test="char-list">
                 <li data-test="char-birth">Date of Birth: {birth}</li>
                 <li data-test="char-death">Date of Death: {death}</li>
